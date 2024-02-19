338755646dbc22e2f3218235d05960d6787ef1fd
EC2 Default User <ec2-user@ip-172-31-10-222.ap-northeast-1.compute.internal>
2024-02-19 14:55:25 +0000

    setting7

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,40 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+#管理者用
+#URL /admin/sign_in
+devise_for :admin, skip: [:registrations, :passwords], controllers: {
+  sessions: "admin/sessions"
+}
+
+#ユーザー用
+#URL /customers/sign_in
+devise_for :customers, skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
 
 
+scope module: :public do
+  root controller: :homes, action: :top
+  get :about, controller: :homes, action: :about
+  get 'posts/tag' => 'posts#tag'
+  resources :posts, only: [:index, :show, :edit, :new, :update, :destroy, :create] do
+    resources :comments#必要のない部分は後でonlyで実施する。
+    resources :greats#必要のない部分は後でonlyで実施する。
+  end
+  get 'customers' => 'customers#show'
+  get 'customers/my_page' => 'customers#my_page'
+  get 'customers/information/edit' => 'customers#edit'
+  patch 'customers/information' => 'customers#update'
+  get 'customers/unsubscribe' => 'customers#unsubscribe', as: 'unsubscribe'
+  patch 'customers/withdraw' => 'customers#withdraw', as: 'withdraw'
+  get 'customers/my_greats_page' => 'customers#my_greats_page'
+end
+
+namespace :admin do
+  root "homes#top"
+  resources :posts, only: [:index, :show, :edit, :update, :destroy]
+  resources :customers, only:[:index, :show, :edit, :update, :destroy]
+  resources :comments#必要のない部分は後でonlyで実施する。
+end
 
 end
